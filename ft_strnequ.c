9f661fe76bf18fa72fe4f1a5b8b96bf6a385583e
Sander Grindheim <sandsalamand@gmail.com>
2018-08-08 22:40:17 -0700

    fixed I think

--- ft_strnequ.c
+++ ft_strnequ.c
@@ -6,7 +6,7 @@
 /*   By: sgrindhe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/04 19:04:57 by sgrindhe          #+#    #+#             */
-/*   Updated: 2018/08/04 19:07:32 by sgrindhe         ###   ########.fr       */
+/*   Updated: 2018/08/08 22:40:02 by sgrindhe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,12 @@
 int		ft_strnequ(char const *s1, char const *s2, size_t n)
 {
 	if (!s1 || !s2)
+	{
+		if (!s1 && !s2)
+			return (1);
 		return (0);
+	}
 	if (ft_strncmp(s1, s2, n) == 0)
 		return (1);
-	else
-		return (0);
+	return (0);
 }
