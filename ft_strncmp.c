4d8f3ba61e6c54926851ecdeb649088404d83024
Sander Grindheim <sandsalamand@gmail.com>
2018-08-09 20:07:00 -0700

    ok

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: sgrindhe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/17 03:43:58 by sgrindhe          #+#    #+#             */
-/*   Updated: 2018/08/08 22:49:34 by sgrindhe         ###   ########.fr       */
+/*   Updated: 2018/08/09 20:06:32 by sgrindhe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n)
 	if (n == 0)
 		return (0);
 	i = 0;
-	while (s1[i] == s2[i] && (s1[i] || s2[i]) && n-- > 0)
+	while (s1[i] == s2[i] && (s1[i] || s2[i]) && n-- > 1)
 	{
 		i++;
 	}
